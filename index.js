f290fb2fd5244cb1e3489dd519332361e816b955
Himanshu Tiwari <htiwari578@gmail.com>
2024-12-23 23:47:33 +0530

    added helmet morgan

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import cors from 'cors'
 import connectDB from './Database/db.js'
 
 
@@ -10,6 +11,11 @@ dotenv.config();
 
 
 const app = express();
+app.use(helmet());
+app.use(morgan("common"));
+app.use(cors());
+app.use(express.json());
+
 
 const PORT = process.env.PORT || 3001;
 
